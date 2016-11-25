d43bc3089c60731c690dd93826471cc402e8ab03
Paulo Cruz (Intelitrader) <paulo.Intelitrader@MIDGARD.LOCAL>
2016-11-25 10:57:07 -0200

    adding vms name at configurations rather than using only index and cache file based, adding force mode at restore function, adding option to retrieve and set switches and network configurations

--- hypy/hvclient.py
+++ hypy/hvclient.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 # coding: utf-8
-
 from winrm import Protocol
 from winrm import Response
 from base64 import b64encode
@@ -40,12 +39,12 @@ def connect(index):
     vm_info = get_vm(index)
     if vm_info != '' and vm_info['State'] != 2 and vm_info['State'] != 9:
         start_vm(index)
-        time.sleep(10)
+        time.sleep(2)
 
-    if platform.uname()[0] == "Windows":
-        freerdp_bin = "wfreerdp.exe"
-    else:
+    if platform.uname()[0] == "Linux":
         freerdp_bin = "xfreerdp"
+    elif platform.uname()[0] == "Windows":
+        freerdp_bin = "wfreerdp.exe"
 
     cmd = [freerdp_bin, '/v:{0}'.format(host), '/vmconnect:{0}'.format(vm_id), '/u:{0}'.format(user),
            '/p:{0}'.format(passw),
@@ -69,10 +68,11 @@ def update_cache(index, new_state):
         index (int): machine index in current cache
         new_state (int): represented by states dict
     """
-    vms[index]['State'] = new_state
+    if vms != None:
+        vms[index]['State'] = new_state
 
-    with open(vms_cache_filename, 'w') as vms_cache_file:
-        json.dump(vms, vms_cache_file, indent=4)
+        with open(vms_cache_filename, 'w') as vms_cache_file:
+            json.dump(vms, vms_cache_file, indent=4)
 
 
 def update_all_cache(force=False):
@@ -86,31 +86,48 @@ def update_all_cache(force=False):
     Returns:
         bool: True for success
     """
+    #if use_cache_file:
     modified = datetime.min
     if os.path.isfile(vms_cache_filename):
         modified = datetime.fromtimestamp(os.path.getmtime(vms_cache_filename))
 
     if modified < datetime.now() - timedelta(hours=int(config['sync_interval'])) or force:
-        ps_script = "Get-VM * | Select Name,Id,State,Uptime | ConvertTo-Json"
-        rs = run_ps(ps_script, server)
 
-        if rs.status_code != 0:
-            print(rs.std_err)
+        vms_json = fetch_data()
+        if vms_json != "":
+            with open(vms_cache_filename, 'w') as vms_cache_file:
+                json.dump(vms_json, vms_cache_file, indent=4)
+        else:
             return False
-
-        vms_json = json.loads(rs.std_out.decode('latin-1'))
-
-        # If there is only one vm, make it a list
-        if type(vms_json) is dict:
-            vms_json = [ vms_json ]
-
-        with open(vms_cache_filename, 'w') as vms_cache_file:
-            json.dump(vms_json, vms_cache_file, indent=4)
 
     return True
 
+def fetch_data():
+    """
+        Fetch vm data from hyper-v server
+        Creates cache file if nonexistent
 
-def load_vms():
+        Returns:
+            dict: json dict contaning hyper-v data
+        """
+    print ("Fetching new data...")
+    ps_script = "Get-VM * | Select Name,Id,State,Uptime | ConvertTo-Json"
+    rs = run_ps(ps_script, server)
+
+    if rs.status_code != 0:
+        print(rs.std_err)
+        return ""
+
+    vms_json = json.loads(rs.std_out.decode('latin-1'))
+
+    # If there is only one vm, make it a list
+    if type(vms_json) is dict:
+        vms_json = [vms_json]
+
+    return vms_json
+
+
+def load_vms(no_cache=False):
     """
     Loads current cache file into memory
 
@@ -120,10 +137,19 @@ def load_vms():
     global vms
 
     try:
-        with open(vms_cache_filename, 'r') as vms_cache_file:
-            vms = json.load(vms_cache_file)
+        if not no_cache:
+            with open(vms_cache_filename, 'r') as vms_cache_file:
+                vms = json.load(vms_cache_file)
+        else:
+            vms = fetch_data()
     except IOError:
         print("Cannot access file {0}".format(vms_cache_filename))
+        return False
+    except ValueError:
+        print ("JSONDecodeError, cannot decode cache file {0}".format(vms_cache_filename))
+        return False
+    except Exception:
+        print ("Error load_vms()")
         return False
 
     return True
@@ -150,16 +176,123 @@ def list_vms():
         print("[{0}] {1} {2} {3}".format(index, state, name, uptime))
 
 
-def list_vm_snaps(vm_index):
+def load_switches():
+    """
+        Fetch switches information from hyper-v server
+        Returns:
+            dict: json dict contaning switches hyper-v data
+        """
+    print ("Fetching switches data...")
+    ps_script = "Get-VMSwitch * | Select Name | ConvertTo-Json"
+    rs = run_ps(ps_script, server)
+
+    if rs.status_code != 0:
+        print(rs.std_err)
+        return ""
+
+    switches_json = json.loads(rs.std_out.decode('latin-1'))
+
+    # If there is only one vm, make it a list
+    if type(switches_json) is dict:
+        switches_json = [switches_json]
+
+    print("-- Virtual Network Switches --")
+    print("{0}".format("Name".ljust(30)))
+    for switch in switches_json:
+        name = str(switch['Name']).ljust(30)
+        print("{0}".format(name))
+
+    return switches_json
+
+
+def set_vm_switch(vm_index, switch, vm_name=""):
+    """
+        Change vm switch based on its name
+
+        Args:
+            vm_index (int): The machine's index generated in the current cache (optional)
+            switch (str): switch name to be changed
+            vm_name (str): vm name that will be changed its switch
+
+        Returns:
+            True - Changed successfully
+            False - Change has failed
+        """
+    if vm_name == "" and vm_index != -1:
+        load_vms()
+        vm_name = vms[vm_index]['Name']
+
+    ps_script = "Connect-VMNetworkAdapter -VMName {0} -SwitchName {1}".format(vm_name, switch)
+    rs = run_ps(ps_script, server)
+
+    if rs.status_code != 0:
+        print(rs.std_err)
+        return False
+
+    return True
+
+
+def get_vm_network(vm_index, vm_name=""):
+    """
+        Get network configuration from vm
+
+        Args:
+            vm_index (int): The machine's index generated in the current cache (optional)
+            vm_name (str): vm name that will be changed its switch
+
+        Returns:
+            json object containing fetched data
+        """
+    if vm_name == "" and vm_index != -1:
+        load_vms()
+        vm_name = vms[vm_index]['Name']
+
+    print ("index: {} name:{}".format(vm_index, vm_name))
+
+    ps_script = "Get-VM -VMName {0} | Select -ExpandProperty networkadapters | Select Name,SwitchName,MacAddress,IPAddresses,Status | ConvertTo-Json".format(
+        vm_name)
+
+    rs = run_ps(ps_script, server)
+
+    if rs.status_code != 0:
+        print(rs.std_err)
+        return False
+
+    try:
+        na_json = json.loads(rs.std_out.decode('latin-1'))
+    except Exception as e:
+        print("Virtual Machine {} has problem with network adapters: {}".format(vm_name, e))
+        return
+
+    # If there is only one network adapter, make it a list
+    if type(na_json) is dict:
+        na_json = [na_json]
+
+    print("-- Virtual Machine Network Adapter --")
+    print("{0} {1} {2} {3} {4}".format("Name".ljust(20), "SwitchName".ljust(20), "MacAddress".ljust(20), "Status".ljust(10), "IPAddresses"))
+    for na in na_json:
+        snapname = str(na['Name']).ljust(20)
+        switchname = str(na['SwitchName']).ljust(20)
+        mac = str(na["MacAddress"]).ljust(20)
+        status = str(na["Status"]).ljust(10)
+        ip = str(na["IPAddresses"])
+        print("{0} {1} {2} {3} {4}".format(snapname, switchname, mac, status, ip))
+
+    return na_json
+
+
+def list_vm_snaps(vm_index, vm_name=""):
     """
     List vm snapshots by vm index
 
     Args:
         vm_index (int): The machine's index generated in the current cache
     """
-    load_vms()
 
-    vm_name = vms[vm_index]['Name']
+    if vm_name == "" and vm_index != -1:
+        load_vms()
+        vm_name = vms[vm_index]['Name']
+
     ps_script = "Get-VMSnapshot -VMName {0} | Select Name,ParentSnapshotName,CreationTime | ConvertTo-Json".format(vm_name)
 
     rs = run_ps(ps_script, server)
@@ -187,7 +320,7 @@ def list_vm_snaps(vm_index):
         print("{0} {1} {2}".format(snapname, parent, creation.strftime("%d/%m/%Y %H:%M:%S")))
 
 
-def restore_vm_snap(vm_index, snap_name):
+def restore_vm_snap(vm_index, snap_name, force=False, vm_name="", no_cache=False):
     """
     Restore virtual machine snapshot
 
@@ -198,9 +331,18 @@ def restore_vm_snap(vm_index, snap_name):
     Returns:
         bool: True if success
     """
-    load_vms()
 
-    vm_name = vms[vm_index]['Name']
+    if vm_name == "" and vm_index != -1:
+        load_vms(no_cache)
+        vm_name = vms[vm_index]['Name']
+
+    if force:
+        vm = get_vm(-1, vm_name)
+        state = states.get(vm['State'], "unknown")
+        print ("Vm in '{}' state".format(state))
+        if "off" in state or "saved" in state:
+            start_vm(-1, vm_name)
+
     ps_script = 'Restore-VMSnapshot -Name "{0}" -VMName {1} -Confirm:$false'.format(snap_name, vm_name)
 
     print('Restoring snapshot "{0}" in {1}'.format(snap_name, vm_name))
@@ -214,7 +356,7 @@ def restore_vm_snap(vm_index, snap_name):
     return True
 
 
-def remove_vm_snapshot(vm_index, snap_name, recursive=False):
+def remove_vm_snapshot(vm_index, snap_name, recursive=False, vm_name=""):
     """
     Deletes a virtual machine checkpoint
 
@@ -227,9 +369,11 @@ def remove_vm_snapshot(vm_index, snap_name, recursive=False):
     Returns:
         bool: True if success
     """
-    load_vms()
 
-    vm_name = vms[vm_index]['Name']
+    if vm_name == "" and vm_index != -1:
+        load_vms()
+        vm_name = vms[vm_index]['Name']
+
     ps_script = 'Remove-VMSnapshot -VMName "{0}" -Name "{1}"'.format(vm_name, snap_name)
     if recursive:
         ps_script += " -IncludeAllChildSnapshots"
@@ -248,7 +392,7 @@ def remove_vm_snapshot(vm_index, snap_name, recursive=False):
     return True
 
 
-def create_vm_snapshot(vm_index, snap_name):
+def create_vm_snapshot(vm_index, snap_name, vm_name=""):
     """
     Create a new snapshot with vm's current state
 
@@ -259,9 +403,11 @@ def create_vm_snapshot(vm_index, snap_name):
     Returns:
         bool: True if success
     """
-    load_vms()
 
-    vm_name = vms[vm_index]['Name']
+    if vm_name == "" and vm_index != -1:
+        load_vms()
+        vm_name = vms[vm_index]['Name']
+
     ps_script = 'Checkpoint-VM -Name "{0}" -SnapshotName "{1}" -Confirm:$false'.format(vm_name, snap_name)
 
     print('Creating snapshot "{0}" in "{1}"'.format(snap_name, vm_name))
@@ -275,16 +421,17 @@ def create_vm_snapshot(vm_index, snap_name):
     return True
 
 
-def get_vm(vm_index):
+def get_vm(vm_index, vm_name=""):
     """
     Gets vm info by index
 
     Args:
         vm_index (int): The machine's index generated in the current cache
     """
-    load_vms()
 
-    vm_name = vms[vm_index]['Name']
+    if vm_name == "" and vm_index != -1:
+        load_vms()
+        vm_name = vms[vm_index]['Name']
 
     ps_script = "Get-VM -Name {0} | Select Name,Id,State | ConvertTo-Json".format(vm_name)
     rs = run_ps(ps_script, server)
@@ -297,7 +444,7 @@ def get_vm(vm_index):
     return vm_json
 
 
-def stop_vm(vm_index, force=False):
+def stop_vm(vm_index, force=False, vm_name=""):
     """
     Stop virtual machine
 
@@ -305,9 +452,11 @@ def stop_vm(vm_index, force=False):
         vm_index (int): The machine's index generated in the current cache
         force (bool): Whether should force shutdown or not
     """
-    load_vms()
 
-    vm_name = vms[vm_index]['Name']
+    if vm_name == "" and vm_index != -1:
+        load_vms()
+        vm_name = vms[vm_index]['Name']
+
     ps_script = "Stop-VM -Name {}".format(vm_name)
     if force:
         ps_script += " -Force"
@@ -324,16 +473,18 @@ def stop_vm(vm_index, force=False):
     return True
 
 
-def resume_vm(vm_index):
+def resume_vm(vm_index, vm_name=""):
     """
     Resume (paused) virtual machine
 
     Args:
         vm_index (int): The machine's index generated in the current cache
     """
-    load_vms()
 
-    vm_name = vms[vm_index]['Name']
+    if vm_name == "" and vm_index != -1:
+        load_vms()
+        vm_name = vms[vm_index]['Name']
+
     ps_script = "Resume-VM -Name {0}".format(vm_name)
 
     print('Resuming VM "{0}"'.format(vm_name))
@@ -348,16 +499,18 @@ def resume_vm(vm_index):
     return True
 
 
-def pause_vm(vm_index):
+def pause_vm(vm_index, vm_name=""):
     """
     Pause virtual machine
 
     Args:
         vm_index (int): The machine's index generated in the current cache
     """
-    load_vms()
 
-    vm_name = vms[vm_index]['Name']
+    if vm_name == "" and vm_index != -1:
+        load_vms()
+        vm_name = vms[vm_index]['Name']
+
     ps_script = "Suspend-VM -Name {0}".format(vm_name)
 
     print('Pausing VM "{0}"'.format(vm_name))
@@ -372,16 +525,18 @@ def pause_vm(vm_index):
     return True
 
 
-def start_vm(vm_index):
+def start_vm(vm_index, vm_name=""):
     """
     Start virtual machine
 
     Args:
         vm_index (int): The machine's index generated in the current cache
     """
-    load_vms()
 
-    vm_name = vms[vm_index]['Name']
+    if vm_name == "" and vm_index != -1:
+        load_vms()
+        vm_name = vms[vm_index]['Name']
+
     ps_script = "Start-VM -Name {0}".format(vm_name)
 
     print('Starting VM "{0}"'.format(vm_name))
@@ -394,6 +549,16 @@ def start_vm(vm_index):
     update_cache(vm_index, 2)
     print("Success")
     return True
+
+
+def validate_vm_index(vm_index, vm_name):
+    if vm_name == "" and vm_index > 0:
+        load_vms()
+        return vms[vm_index]['Name']
+    elif vm_name != "" and vm_index == -1:
+        return vm_name
+    else:
+        raise Exception("Vm name '{}' is invalid!".format(vm_name))
 
 
 def setup(configp):
